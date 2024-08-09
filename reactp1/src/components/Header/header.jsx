9f6e4b4cf77e94924dbbde564174f4e42f4654c7
Apurva <117301920+apurvak3@users.noreply.github.com>
2024-08-09 23:13:54 +0530

    Added nav bar

--- reactp1/src/components/Header/header.jsx
+++ reactp1/src/components/Header/header.jsx
@@ -1,31 +1,50 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "./header.css";
 
 function Header() {
   return (
-    <>
-      <nav>
-        <div className="video-container">
-          <video src="Air (1).mp4" autoPlay loop muted></video>
-        </div>
-        <div className="flex items-center lg:order-2">
-          <Link
-            to="#"
-            className="text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
-          >
-            Log in
-          </Link>
-          <Link
-            to="#"
-            className="text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
-          >
-            Get started
-          </Link>
-        </div>
-      </nav>
-    </>
+    <nav className="header-container">
+      <div className="video-container">
+        <video src="Air (1).mp4" autoPlay loop muted></video>
+      </div>
+      <div className="nav-links">
+        <ul className="flex">
+          <li>
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                `nav-link ${isActive ? "active" : ""}`
+              }
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/about"
+              className={({ isActive }) =>
+                `nav-link ${isActive ? "active" : ""}`
+              }
+            >
+              Blog
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/contact"
+              className={({ isActive }) =>
+                `nav-link ${isActive ? "active" : ""}`
+              }
+            >
+              Support
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 }
 
 export default Header;
+
