3042f53e25dfbbb912fc96a4474bc355e6b17d7c
Lokendra Dhakrey <lokysthakur123@gmail.com>
2024-08-09 16:36:32 +0530

    Some changes in navbar and home.

--- reactp1/src/App.jsx
+++ reactp1/src/App.jsx
@@ -1,13 +1,10 @@
-import { useState } from "react";
 import "./App.css";
-import Header from "./components/Header/header";
+import Home from "./components/Home";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <Header />
+      <Home />
     </>
   );
 }
---
+++ reactp1/src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./Header";
+import "../components/home.css";
+
+function Home() {
+  return (
+    <>
+      <Header/>
+    </>
+  );
+}
+
+export default Home;
